6d04c6db7e0c87b0819ec8da05539f16d4f178bc
Xiao7 <63487309+Allen-98@users.noreply.github.com>
2022-04-14 16:51:33 +0800

    ROG update
    基础框架搭建， UI模板完成

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
+    public YuHun yuhun;
+
+    public TMP_Text[] names;
+    public TMP_Text[] values;
+
+    public Button buyButton;
+    public Button addThreeButton;
+    public Button maxButton;
+    public Button restartButton;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
     }
 
     // Update is called once per frame
@@ -15,4 +30,36 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    public void BuyOne()
+    {
+        names[0].SetText("速度");
+        names[1].SetText("速度");
+        names[2].SetText("攻击加成");
+        names[3].SetText("暴击");
+        names[4].SetText("暴击伤害");
+
+    }
+
+
+
+
+
+    public void Reset()
+    {
+        names[0].SetText("主属性名称");
+        names[1].SetText("副属性名称");
+        names[2].SetText("副属性名称");
+        names[3].SetText("副属性名称");
+        names[4].SetText("副属性名称");
+
+        values[0].SetText("数值");
+        values[1].SetText("数值");
+        values[2].SetText("数值");
+        values[3].SetText("数值");
+        values[4].SetText("数值");
+
+    }
+
+
 }
---
+++ Assets/Scripts/YuHun.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YuHun : MonoBehaviour
+{
+    public Dictionary<int, YuhunInfo> singleYuhun;
+
+    /// <summary>
+    /// 生成主属性
+    /// </summary>
+    public YuhunInfo GenMainProperty(int type)
+    {
+        YuhunInfo kvp = new YuhunInfo("主属性1", 2);
+
+
+        return kvp;
+    }
+    /// <summary>
+    /// 生成fu属性
+    /// </summary>
+    public YuhunInfo GenSubProperty(int type)
+    {
+        YuhunInfo kvp = new YuhunInfo("附属性1", 2);
+
+
+        return kvp;
+    }
+
+
+    public void GenNewYuhun(int type)
+    {
+        YuhunInfo main = GenMainProperty(type);
+        singleYuhun.Add(0,main);
+        int subNum = Random.Range(2, 5);
+        for (int i = 0; i < subNum; i++)
+        {
+            YuhunInfo sub = GenSubProperty(type);
+            singleYuhun.Add(i+1, sub);
+        }
+    }
+
+    public YuhunInfo GetYuhun(int pos)
+    {
+        YuhunInfo yuhunInfo;
+        yuhunInfo = singleYuhun[pos];
+        return yuhunInfo;
+    }
+
+    public struct YuhunInfo
+    {
+        public string name;
+        public float value;
+
+        public YuhunInfo(string name, float value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
